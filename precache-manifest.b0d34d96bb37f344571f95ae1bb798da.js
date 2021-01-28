6e52ed02abe322dc41015aee58ae34d7adbbe24b
Bruno Contreras <BSBContreras@gmail.com>
2021-01-28 14:21:39 -0300

    First build

---
+++ precache-manifest.b0d34d96bb37f344571f95ae1bb798da.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fa54c105ac9cacc1bdb9eb157a45046",
+    "url": "/index.html"
+  },
+  {
+    "revision": "968f0713e24685cac80b",
+    "url": "/static/css/main.43b56764.chunk.css"
+  },
+  {
+    "revision": "aea27216bf6a718bbb02",
+    "url": "/static/js/2.825bea2e.chunk.js"
+  },
+  {
+    "revision": "700e5cec03fed638d98f41eff63ff498",
+    "url": "/static/js/2.825bea2e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "968f0713e24685cac80b",
+    "url": "/static/js/main.1da4120c.chunk.js"
+  },
+  {
+    "revision": "810b7eddbba2845a5756",
+    "url": "/static/js/runtime-main.4e64986e.js"
+  },
+  {
+    "revision": "315809f60781ef532cd024299ec84afa",
+    "url": "/static/media/completed_steps.315809f6.svg"
+  },
+  {
+    "revision": "7da78e06f50d264b313aa988cd39ecc7",
+    "url": "/static/media/instant_information.7da78e06.svg"
+  },
+  {
+    "revision": "e95c4387b982948615cb5d0e10ef1270",
+    "url": "/static/media/select_website.e95c4387.svg"
+  }
+]);
